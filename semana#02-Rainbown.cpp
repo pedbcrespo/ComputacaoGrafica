873ba3792b02c8b42d89d5f8f505183f16bca559
Pedro Crespo <pedbcrespo@gmail.com>
2022-04-12 00:08:10 -0300

    tarefa das cores

---
+++ semana#02-Rainbown.cpp
@@ -0,0 +1,21 @@
+#include "Image.h"
+#include "math.h"
+#include "vec.h"
+#include<iostream>
+
+int main() {
+    int w = 600, h = 600;
+
+    Image img(w, h);
+    img.fill(white);
+
+    for (int x = 0; x < w; x++) {
+        for (int y = 0; y < h; y++) {
+            img(x, y) = bilerp((float)x/w, (float)y/h, blue, green, orange, purple);
+        }
+    }
+
+    img.savePNG("rainbown.png");
+
+    return 0;
+}
